e5ab79da852692a03df0622563fa40d7b33441ad
ayuzahira18 <70963444+ayuzahira18@users.noreply.github.com>
2020-09-08 23:07:37 +0700

    Ayu Zahira Gunawan
    AyuZahiraGunawan_XII RPL B_UTSspet2020

---
+++ karakter.java
@@ -0,0 +1,21 @@
+package UlanganPBO;
+
+public class karakter {
+
+  public static void main(String[] args) {
+    String str = "A,Y,U,-,Z,A,H,I,R,A";
+    System.out.println(str.charAt(0));
+    System.out.println(str.charAt(2));
+    System.out.println(str.charAt(4));
+    System.out.println(str.charAt(6));
+    System.out.println(str.charAt(8));
+    System.out.println(str.charAt(10));
+    System.out.println(str.charAt(12));
+    System.out.println(str.charAt(14));
+    System.out.println(str.charAt(16));
+    System.out.println(str.charAt(18));
+
+    System.out.println(
+        "Mungkin masih banyak kurangnya semoga bermanfaat" + ",Semangat menuju impian Semoga Virus segera tiada🤗");
+  }
+}
